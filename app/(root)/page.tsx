0feb6289b17d30e4d5094f784dd80eb905f03382
Mark Ilchenko <markilchenko18.hillel@gmail.com>
2024-03-03 09:06:19 +0200

    fixed

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -10,27 +10,42 @@ export default async function Home() {
 
   return (
     <>
-      <h1 className="head-text text-left">Home Test</h1>
+      <h1 className="head-text text-left">Home</h1>
 
       <section className="mt-9 flex flex-col gap-10">
         {result.posts.length === 0 ? (
           <p className="no-result">No threads found</p>
         ) : (
           <>
-            {result.posts.map((post) => (
+            {result.posts.map((post) => {
               // <p>No result</p>
-              <ThreadCard 
-                key={post._id} 
-                id={post._id} 
-                currentUserId={user?.id || ""} 
-                parentId={post.parentId} 
-                content={post.text} 
-                author={post.author} 
-                community={post.community} 
-                createdAt={post.createdAt} 
-                comments={post.children} 
-              />
-            ))}
+              // <ThreadCard 
+              //   key={post._id} 
+              //   id={post._id} 
+              //   currentUserId={user?.id || ""} 
+              //   parentId={post.parentId} 
+              //   content={post.text} 
+              //   author={post.author} 
+              //   community={post.community} 
+              //   createdAt={post.createdAt} 
+              //   comments={post.children} 
+              // />
+              return !post.firstDay || !post.lastDay ? (
+                <ThreadCard 
+                  key={post._id} 
+                  id={post._id} 
+                  currentUserId={user?.id || ""} 
+                  parentId={post.parentId} 
+                  content={post.text} 
+                  author={post.author} 
+                  community={post.community} 
+                  createdAt={post.createdAt} 
+                  comments={post.children} 
+                />
+              ) : (
+                <p>No result</p>
+              );
+            })}
           </>
         )}
       </section>
